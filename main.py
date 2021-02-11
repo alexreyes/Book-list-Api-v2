17fdfb5d4f9f5946b95c38c8db4b48e5d9439690
Alex Reyes <reyesruizalex@gmail.com>
2021-02-11 23:33:27 +0000

    API Finished! Fully working :)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from replit import db
 from goodreadsScrape import get_books
 
@@ -7,7 +8,12 @@ app = Flask('app')
 # Support for special characters like accents
 app.config['JSON_AS_ASCII'] = False
 
+# CORS Support
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/')
+@cross_origin()
 def index():
   if 'year' in request.args:
     year = request.args['year']
@@ -18,6 +24,7 @@ def index():
     return 'This is my personal books API. Written by Alex Reyes for use in www.alexreyes.xyz. Please request a specific year. For example: /?year=2021'
 
 @app.route('/', methods=["POST"])
+@cross_origin()
 def update_list(): 
   if 'year' in request.args:
     year = request.args['year']
